ea02afed5a033e1ca348a6759cdd476f81a8877c
Marc Neudert <marc.neudert@gmail.com>
2014-05-20 21:02:55 +0200

    s/device_detection/d14n/g

--- src/ngx_http_device_detection_module.c
+++ src/ngx_http_device_detection_module.c
@@ -6,72 +6,72 @@
 typedef struct {
     ngx_flag_t  loaded;
     ngx_str_t   yaml;
-} ngx_http_device_detection_conf_t;
+} ngx_http_d14n_conf_t;
 
 
-static ngx_int_t ngx_http_device_detection_loaded_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data);
-static ngx_int_t ngx_http_device_detection_type_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data);
+static ngx_int_t ngx_http_d14n_loaded_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data);
+static ngx_int_t ngx_http_d14n_type_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data);
 
-static ngx_int_t ngx_http_device_detection_add_variables(ngx_conf_t *cf);
-static void *ngx_http_device_detection_create_loc_conf(ngx_conf_t *cf);
-static char *ngx_http_device_detection_merge_loc_conf(ngx_conf_t *cf,void *parent, void *child);
-static char *ngx_http_device_detection_yaml_value(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_http_d14n_add_variables(ngx_conf_t *cf);
+static void *ngx_http_d14n_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_d14n_merge_loc_conf(ngx_conf_t *cf,void *parent, void *child);
+static char *ngx_http_d14n_yaml_value(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 
-static ngx_command_t  ngx_http_device_detection_commands[] = {
+static ngx_command_t  ngx_http_d14n_commands[] = {
     { ngx_string("device_detection_yaml"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
-      ngx_http_device_detection_yaml_value,
+      ngx_http_d14n_yaml_value,
       NGX_HTTP_LOC_CONF_OFFSET,
-      offsetof(ngx_http_device_detection_conf_t, yaml),
+      offsetof(ngx_http_d14n_conf_t, yaml),
       NULL },
 
     ngx_null_command
 };
 
 
-static ngx_http_module_t  ngx_http_device_detection_module_ctx = {
-    ngx_http_device_detection_add_variables,    /* preconfiguration */
-    NULL,                                       /* postconfiguration */
+static ngx_http_module_t  ngx_http_d14n_module_ctx = {
+    ngx_http_d14n_add_variables,    /* preconfiguration */
+    NULL,                           /* postconfiguration */
 
-    NULL,                                       /* create main configuration */
-    NULL,                                       /* init main configuration */
+    NULL,                           /* create main configuration */
+    NULL,                           /* init main configuration */
 
-    NULL,                                       /* create server configuration */
-    NULL,                                       /* merge server configuration */
+    NULL,                           /* create server configuration */
+    NULL,                           /* merge server configuration */
 
-    ngx_http_device_detection_create_loc_conf,  /* create location configuration */
-    ngx_http_device_detection_merge_loc_conf    /* merge location configuration */
+    ngx_http_d14n_create_loc_conf,  /* create location configuration */
+    ngx_http_d14n_merge_loc_conf    /* merge location configuration */
 };
 
 
 ngx_module_t  ngx_http_device_detection_module = {
     NGX_MODULE_V1,
-    &ngx_http_device_detection_module_ctx,  /* module context */
-    ngx_http_device_detection_commands,     /* module directives */
-    NGX_HTTP_MODULE,                        /* module type */
+    &ngx_http_d14n_module_ctx,      /* module context */
+    ngx_http_d14n_commands,         /* module directives */
+    NGX_HTTP_MODULE,                /* module type */
 
-    NULL,                                   /* init master */
-    NULL,                                   /* init module */
-    NULL,                                   /* init process */
-    NULL,                                   /* init thread */
+    NULL,                           /* init master */
+    NULL,                           /* init module */
+    NULL,                           /* init process */
+    NULL,                           /* init thread */
 
-    NULL,                                   /* exit thread */
-    NULL,                                   /* exit process */
-    NULL,                                   /* exit master */
+    NULL,                           /* exit thread */
+    NULL,                           /* exit process */
+    NULL,                           /* exit master */
     NGX_MODULE_V1_PADDING
 };
 
 
-static ngx_http_variable_t  ngx_http_device_detection_vars[] = {
+static ngx_http_variable_t  ngx_http_d14n_vars[] = {
     { ngx_string("device_detection_loaded"),
       NULL,
-      ngx_http_device_detection_loaded_variable,
+      ngx_http_d14n_loaded_variable,
       0, 0, 0 },
 
     { ngx_string("device_detection_type"),
       NULL,
-      ngx_http_device_detection_type_variable,
+      ngx_http_d14n_type_variable,
       0, 0, 0 },
 
     { ngx_null_string, NULL, NULL, 0, 0, 0 }
@@ -79,11 +79,11 @@ static ngx_http_variable_t  ngx_http_device_detection_vars[] = {
 
 
 static ngx_int_t
-ngx_http_device_detection_loaded_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data)
+ngx_http_d14n_loaded_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data)
 {
     size_t                            len;
     const char                       *val;
-    ngx_http_device_detection_conf_t *lcf;
+    ngx_http_d14n_conf_t *lcf;
 
     lcf = ngx_http_get_module_loc_conf(r, ngx_http_device_detection_module);
 
@@ -112,7 +112,7 @@ ngx_http_device_detection_loaded_variable(ngx_http_request_t *r, ngx_http_variab
 
 
 static ngx_int_t
-ngx_http_device_detection_type_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data)
+ngx_http_d14n_type_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data)
 {
     const char *val = "undetected";
     size_t      len = ngx_strlen(val);
@@ -135,11 +135,11 @@ ngx_http_device_detection_type_variable(ngx_http_request_t *r, ngx_http_variable
 
 
 static ngx_int_t
-ngx_http_device_detection_add_variables(ngx_conf_t *cf)
+ngx_http_d14n_add_variables(ngx_conf_t *cf)
 {
     ngx_http_variable_t  *var, *v;
 
-    for (v = ngx_http_device_detection_vars; v->name.len; v++) {
+    for (v = ngx_http_d14n_vars; v->name.len; v++) {
         var = ngx_http_add_variable(cf, &v->name, v->flags);
 
         if (NULL == var) {
@@ -155,11 +155,11 @@ ngx_http_device_detection_add_variables(ngx_conf_t *cf)
 
 
 static void *
-ngx_http_device_detection_create_loc_conf(ngx_conf_t *cf)
+ngx_http_d14n_create_loc_conf(ngx_conf_t *cf)
 {
-    ngx_http_device_detection_conf_t  *conf;
+    ngx_http_d14n_conf_t  *conf;
 
-    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_device_detection_conf_t));
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_d14n_conf_t));
 
     if (NULL == conf) {
         return NULL;
@@ -172,10 +172,10 @@ ngx_http_device_detection_create_loc_conf(ngx_conf_t *cf)
 
 
 static char *
-ngx_http_device_detection_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+ngx_http_d14n_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 {
-    ngx_http_device_detection_conf_t *prev = parent;
-    ngx_http_device_detection_conf_t *conf = child;
+    ngx_http_d14n_conf_t *prev = parent;
+    ngx_http_d14n_conf_t *conf = child;
 
     ngx_conf_merge_off_value(conf->loaded, prev->loaded, 0);
     ngx_conf_merge_str_value(conf->yaml, prev->yaml, "");
@@ -185,11 +185,11 @@ ngx_http_device_detection_merge_loc_conf(ngx_conf_t *cf, void *parent, void *chi
 
 
 static char *
-ngx_http_device_detection_yaml_value(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+ngx_http_d14n_yaml_value(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     ngx_str_t *value;
 
-    ngx_http_device_detection_conf_t *lcf = conf;
+    ngx_http_d14n_conf_t *lcf = conf;
 
     if (lcf->yaml.data) {
         return "is duplicate";
